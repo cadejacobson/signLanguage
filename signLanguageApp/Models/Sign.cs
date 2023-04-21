5915b3f003aa88694eec521ff9720550d7785784
Cade Jacobson <cade.jacobson@mines.sdsmt.edu>
2023-04-21 10:22:43 -0600

    Ref #4 Begin the sign model

---
+++ signLanguageApp/Models/Sign.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace signLanguageApp.Models
+{
+    public class Sign
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Image { get; set; }
+    }
+}
