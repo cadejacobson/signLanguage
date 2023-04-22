52f1f5c44185615d64b9f6db95444157e00b4ef8
Cade Jacobson <cade.jacobson@mines.sdsmt.edu>
2023-04-21 18:00:05 -0600

    Ref #8 Add collection data type for game system

--- signLanguageApp/ViewModels/SignViewModel.cs
+++ signLanguageApp/ViewModels/SignViewModel.cs
@@ -16,12 +16,14 @@ namespace signLanguageApp.ViewModels
                 Id = sign.Id;
                 Name = sign.Name;
                 Image = sign.Image;
+                Collection = sign.Collection;
             }
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
+        public string Collection { get; set; }
 
         public IEnumerable<Sign> Signs { get; set; }
     }
